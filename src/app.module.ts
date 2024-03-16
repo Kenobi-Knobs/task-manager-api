8dbfa2745ea093726d858138dbf5ee256b4927ab
Konchatniy Vitaliy <konchatniyvitaliy@gmail.com>
2024-03-16 14:41:56 +0200

    connect app to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
+import * as fs from 'fs';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const config = JSON.parse(fs.readFileSync('appconfig.json', 'utf8').toString());
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: config.mongoUri,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
