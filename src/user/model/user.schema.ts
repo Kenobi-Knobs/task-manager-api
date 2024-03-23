926ef59c268288f7734609deadcd37685606e925
Konchatniy Vitaliy <konchatniyvitaliy@gmail.com>
2024-03-23 15:07:30 +0200

    update user schema

--- src/user/model/user.schema.ts
+++ src/user/model/user.schema.ts
@@ -1,10 +1,13 @@
 ﻿import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
 import { HydratedDocument } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 
 @Schema()
 export class User {
+  _id: mongoose.Types.ObjectId;
+
   @Prop()
   name: string;
 
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,14 +3,18 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './model/user.schema';
 import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
-  async create(user: User): Promise<User> {
-    const hashedPassword = await bcrypt.hash(user.password, 10);
-    const newUser = new this.userModel({ ...user, password: hashedPassword });
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    const newUser = new this.userModel({
+      ...createUserDto,
+      password: hashedPassword,
+    });
     return newUser.save();
   }
 
