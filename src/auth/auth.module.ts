fe81ed5aa39c96209488ac5c73d532159719aa28
Konchatniy Vitaliy <konchatniyvitaliy@gmail.com>
2024-03-23 15:30:57 +0200

    add jwt generation, update config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,24 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import * as fs from 'fs';
+
+const config = JSON.parse(
+  fs.readFileSync('./appconfig.json', 'utf8').toString(),
+);
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      global: true,
+      secret: config.jwtSecret,
+      signOptions: { expiresIn: config.jwtExpire },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,19 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
-  async signIn(email: string, password: string): Promise<any> {
+  async signIn(
+    email: string,
+    password: string,
+  ): Promise<{ acces_token: string }> {
     const user = await this.userService.findOne(email);
     if (!user) {
       throw new UnauthorizedException('Invalid credentials');
@@ -16,9 +23,10 @@ export class AuthService {
     if (!isPasswordValid) {
       throw new UnauthorizedException('Invalid credentials');
     }
-    // TODO: Generate a JWT and return it here
-    // instead of the user object
 
-    return user;
+    const payload = { sub: user._id, email: user.email };
+    const token = await this.jwtService.signAsync(payload);
+
+    return { acces_token: token };
   }
 }
