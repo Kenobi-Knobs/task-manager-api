3817761103ffa880f027eb8ce69b312e19bf0eeb
Konchatniy Vitaliy <konchatniyvitaliy@gmail.com>
2024-03-23 21:02:42 +0200

    add auth guard

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,42 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as fs from 'fs';
+import { Request } from 'express';
+
+const config = JSON.parse(
+  fs.readFileSync('./appconfig.json', 'utf8').toString(),
+);
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) {
+      throw new UnauthorizedException('Authorization token is required');
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: config.jwtSecret,
+      });
+      request['user'] = payload;
+    } catch (error) {
+      throw new UnauthorizedException('Invalid token');
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
