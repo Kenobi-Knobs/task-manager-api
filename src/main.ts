35ba117d3ae586fe97566f52f070827884071019
Konchatniy Vitaliy <konchatniyvitaliy@gmail.com>
2024-03-27 20:10:28 +0200

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Task Manager API')
+    .setDescription('Task Manager API for managing tasks and projects')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
