205d7f8108fd2980210bb59d4e9e18bd1ac8b663
Konchatniy Vitaliy <konchatniyvitaliy@gmail.com>
2024-03-30 17:03:09 +0200

    add auth and user services tests, fix app controller test

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UserService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { UnauthorizedException } from '@nestjs/common';
+
+const correctEmail = 'email@gmail.com';
+const correctPassword = 'password';
+const token = 'generated token';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UserService,
+          useValue: {
+            findOne: jest.fn().mockImplementation(async (email: string) => {
+              if (email === correctEmail) {
+                return {
+                  email: correctEmail,
+                  password: await bcrypt.hash(correctPassword, 10),
+                };
+              }
+              return null;
+            }),
+          },
+        },
+        {
+          provide: JwtService,
+          useValue: {
+            signAsync: jest.fn().mockResolvedValue(token),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
@@ -14,5 +44,34 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('sign in', () => {
+    it('should return token if credentials are valid', async () => {
+      const result = await service.signIn(correctEmail, correctPassword);
+      expect(result).toEqual(token);
+    });
+
+    it('should throw an error if email invalid', async () => {
+      const invalidEmail = 'wrong@gmail.com';
+      await expect(
+        service.signIn(invalidEmail, correctPassword),
+      ).rejects.toThrow(UnauthorizedException);
+    });
+
+    it('should throw an error if password invalid', async () => {
+      const invalidPassword = 'wrong-password';
+      await expect(
+        service.signIn(correctEmail, invalidPassword),
+      ).rejects.toThrow(UnauthorizedException);
+    });
+
+    it('should throw an error if credentials are invalid', async () => {
+      const invalidEmail = 'wrong@gmail.com';
+      const invalidPassword = 'wrong-password';
+      await expect(
+        service.signIn(invalidEmail, invalidPassword),
+      ).rejects.toThrow(UnauthorizedException);
+    });
   });
 });
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './users.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { User } from './model/user.schema';
+
+class UserModel {
+  constructor(private data) {}
+  save = jest.fn().mockResolvedValue(this.data);
+  static findOne = jest.fn().mockReturnThis();
+  static exec = jest.fn();
+}
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        { provide: getModelToken(User.name), useValue: UserModel },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
@@ -14,5 +26,31 @@ describe('UserService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a user with hashed password', async () => {
+      const name = 'User name';
+      const email = 'email@gmail.com';
+      const password = 'password';
+
+      const result = await service.create(name, email, password);
+      expect(result.name).toEqual(name);
+      expect(result.email).toEqual(email);
+      expect(result.password).not.toEqual(password);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should find a user by email', async () => {
+      const email = 'email@gmail.com';
+      const user = { name: 'User name', email, password: 'hashed-password' };
+
+      UserModel.exec.mockResolvedValueOnce(user);
+
+      const result = await service.findOne(email);
+      expect(UserModel.findOne).toHaveBeenCalledWith({ email });
+      expect(result).toEqual(user);
+    });
   });
 });
