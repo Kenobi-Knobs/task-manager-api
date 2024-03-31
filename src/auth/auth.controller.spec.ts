a5fcc014841a494808bfd03936ad14b17ecbc9e7
Konchatniy Vitaliy <konchatniyvitaliy@gmail.com>
2024-03-31 14:51:12 +0300

    add test for user and auth controllers

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,18 +1,64 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from '../users/users.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { User } from '../users/model/user.schema';
+import { LoginResponseDto } from './dto/login-response.dto';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        AuthService,
+        JwtService,
+        UserService,
+        {
+          provide: getModelToken(User.name),
+          useValue: {
+            new: jest.fn(),
+            constructor: jest.fn(),
+            find: jest.fn(),
+            findOne: jest.fn(),
+            exec: jest.fn(),
+            save: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
+    service = module.get<AuthService>(AuthService);
     controller = module.get<AuthController>(AuthController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('login', () => {
+    it('should return a token', async () => {
+      const loginDto = {
+        email: 'email@gmail.com',
+        password: 'password',
+      };
+
+      jest.spyOn(service, 'signIn').mockImplementation(async () => {
+        return Promise.resolve('token');
+      });
+
+      const result = await controller.login(loginDto);
+
+      expect(service.signIn).toHaveBeenCalledWith(
+        loginDto.email,
+        loginDto.password,
+      );
+
+      expect(result).toEqual(new LoginResponseDto('token'));
+    });
   });
 });
--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,18 +1,82 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './users.controller';
+import { UserService } from './users.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { User } from './model/user.schema';
+import { CreateUserResponseDto } from './dto/create-user-response.dto';
+import { ConflictException } from '@nestjs/common';
 
 describe('UserController', () => {
   let controller: UserController;
+  let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [
+        UserService,
+        {
+          provide: getModelToken(User.name),
+          useValue: {
+            new: jest.fn(),
+            constructor: jest.fn(),
+            find: jest.fn(),
+            findOne: jest.fn(),
+            exec: jest.fn(),
+            save: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    service = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a user', async () => {
+      const createUserDto = {
+        name: 'John Doe',
+        email: 'email@gmail.com',
+        password: 'password',
+      };
+
+      jest.spyOn(service, 'create').mockImplementation(async () => {
+        return Promise.resolve(new User());
+      });
+
+      const result = await controller.create(createUserDto);
+
+      expect(service.create).toHaveBeenCalledWith(
+        createUserDto.name,
+        createUserDto.email,
+        createUserDto.password,
+      );
+      expect(result).toEqual(
+        new CreateUserResponseDto(
+          'User [' + createUserDto.email + '] created successfully',
+        ),
+      );
+    });
+
+    it('should throw a ConflictException', () => {
+      const createUserDto = {
+        name: 'John Doe',
+        email: 'email@gmail.com',
+        password: 'password',
+      };
+
+      jest.spyOn(service, 'create').mockImplementation(async () => {
+        throw { code: 11000 };
+      });
+
+      expect(controller.create(createUserDto)).rejects.toThrow(
+        ConflictException,
+      );
+    });
   });
 });
